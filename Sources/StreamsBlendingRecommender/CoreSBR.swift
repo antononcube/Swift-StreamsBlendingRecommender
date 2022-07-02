10badc6ec204566835839f2dc3a586ccc7aa852e
antononcube <antononcube@posteo.net>
2022-07-02 07:10:07 -0400

    fix:Preparation for tag-inverse-indexes making.

--- Sources/StreamsBlendingRecommender/CoreSBR.swift
+++ Sources/StreamsBlendingRecommender/CoreSBR.swift
@@ -3,13 +3,13 @@ public class CoreSBR {
     //========================================================
     // Data members
     //========================================================
-    var SMRMatrix: [[String: Any]] = []
+    var SMRMatrix: [[String: String]] = []
     var itemInverseIndexes: [String: [String : Double]] = [:]
     var tagInverseIndexes: [String: [String : Double]] = [:]
     var tagTypeToTags: [String: [String]] = [:]
     var globalWeights: [String: Double] = [:]
-    var knownTags: Set<Character> = Set<Character>()
-    var knownItems: Set<Character> = Set<Character>()
+    var knownTags: Set<String> = Set<String>()
+    var knownItems: Set<String> = Set<String>()
     
     //========================================================
     /// Initialization
@@ -63,6 +63,42 @@ public class CoreSBR {
     //========================================================
     /// Make the inverse indexes that correspond to the SMR matrix.
     public func makeTagInverseIndexes() -> Bool {
+        
+        // Split into a hash by tag type.
+        var inverseIndexGroups = Dictionary(grouping: self.SMRMatrix, by: { r in r["TagType"]! })
+
+        // For each tag type split into hash by Value.
+        var inverseIndexesPerTagType = inverseIndexGroups.mapValues { it0 in Dictionary(grouping: it0, by: { $0["Value"] }) }
+
+        // Re-make each array of hashes into a hash.
+        // println( inverseIndexesPerTagType.mapValues{ typeRec -> typeRec.value.mapValues{ it.value.size } } )
+
+        /*
+        var inverseIndexesPerTagType2 = inverseIndexesPerTagType.mapValues { typeRec in
+            typeRec.mapValues { tagRec in
+                tagRec.value.associateBy(
+                    { $0["Item"] },
+                    { Double($0["Weight"]) })
+            }
+        }
+
+        // Derive the tag type to tags hash map.
+        self.tagTypeToTags = inverseIndexesPerTagType2.mapValues { it in Set(it.value.keys) }
+
+        // Flatten the inverse index groups.
+        self.tagInverseIndexes = [String: [String : Double]]();
+        for (k, v) in inverseIndexesPerTagType2 {
+            self.tagInverseIndexes.merge(v) { (_, new) in new }
+        }
+
+        // Assign known tags.
+        self.knownTags = Set(self.tagInverseIndexes.keys);
+
+        // We make sure item inverse indexes are empty.
+        self.itemInverseIndexes = [String: [String : Double]]();
+        */
+        
         return false
     }
+    
 }
