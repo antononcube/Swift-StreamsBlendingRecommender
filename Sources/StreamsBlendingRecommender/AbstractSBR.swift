eca853c32979a15fa0f4261b6426458d442c24f4
antononcube <antononcube@posteo.net>
2022-07-16 11:18:59 -0400

    refactor:Defined AbstractSBR as a protocol with basic SBR methods.

--- Sources/StreamsBlendingRecommender/AbstractSBR.swift
+++ Sources/StreamsBlendingRecommender/AbstractSBR.swift
@@ -7,7 +7,51 @@
 
 import Foundation
 
-class AbstractSBR {
+protocol AbstractSBR {
     
-
+    //========================================================
+    //Profile
+    //========================================================
+    func profile( items: [String],
+                  normalize: Bool,
+                  warn: Bool )
+    -> [Dictionary<String, Double>.Element]
+    
+    func profile( items: [String : Double],
+                  normalize: Bool,
+                  warn: Bool)
+    -> [Dictionary<String, Double>.Element]
+    
+    //========================================================
+    //Recommend
+    //========================================================
+    func recommend( items: [String],
+                    nrecs: Int,
+                    normalize: Bool,
+                    warn: Bool )
+    -> [Dictionary<String, Double>.Element]
+    
+    
+    func recommend( items: [String : Double],
+                    nrecs: Int,
+                    normalize: Bool,
+                    warn: Bool )
+    -> [Dictionary<String, Double>.Element]
+    
+    //========================================================
+    //Recommend by profile
+    //========================================================
+    func recommendByProfile( prof: [String],
+                             nrecs: Int,
+                             normSpec: String,
+                             warn: Bool )
+    -> [Dictionary<String, Double>.Element]
+    
+    
+    func recommendByProfile( prof: [String : Double],
+                             nrecs: Int,
+                             normSpec: String,
+                             normalize: Bool,
+                             warn: Bool)
+    -> [Dictionary<String, Double>.Element]
 }
